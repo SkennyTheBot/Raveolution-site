5fc6efe88601c732f632ca3cccf85eb31594a690
SkennyTheBot <75219661+SkennyTheBot@users.noreply.github.com>
2022-01-25 19:17:02 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+var server_name = "Raveolution";
+var server_ip = "51.89.8.23";
+var server_port = 25676; /*25565*/
+var ip_copied = "IP copied!";
+
+var firefly_count = 20;
+
+staff("JustRejv", "828e5362-c20c-4d26-a6ad-de99505e1951", "Owner");
+staff("Goksi88", "4ee3d1a7-2569-4ca4-ba0b-83e527b8d186", "Developer");
+staff("Karrakaz", "afa53249-c66b-40af-894b-6b89a083aba3", "Developer");
+staff("DoktorSycolix", "0f46ada2-cfec-4871-aafd-3c37ffd5f8b2", "Manager");
+staff("RealMaki", "9fdc00e0-97ca-4600-9e45-f9a27982950a", "Manager");
+staff("JedanJeLeXa", "c671c44d-a56d-4088-a7f9-8b9a23fc59fb", "Manager");
+staff("TheLuka", "0b190e82-fe6f-40d3-80d3-efc87c93b57b", "Admin");
+staff("Edo37PvP", "9fdc000-97ca-4600-9e45-f9a27982950a", "Admin");
+staff("Cuckovic", "9fdc000-97ca-4600-9e45-f9a27982950a", "Senior Mod");
+
+
+vote("Planet-Minecraft", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://planetminecraft.com");
+vote("Minecraft Servers", "https://images.app.goo.gl/oTegq49com6d3o7o9", "https://minecraftservers.org/");
+vote("Top Minecraft Servers", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://topminecraftservers.org/");
