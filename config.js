f0d0f5d6525c7d516a76b17fc65ccecf77f21129
SkennyTheBot <75219661+SkennyTheBot@users.noreply.github.com>
2024-05-24 11:29:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,16 +5,13 @@ var ip_copied = "IP copied!";
 
 var firefly_count = 20;
 
-staff("JustRejv", "828e5362-c20c-4d26-a6ad-de99505e1951", "Owner");
-staff("Goksi88", "4ee3d1a7-2569-4ca4-ba0b-83e527b8d186", "Developer");
-staff("Karrakaz", "afa53249-c66b-40af-894b-6b89a083aba3", "Developer");
-staff("DoktorSycolix", "0f46ada2-cfec-4871-aafd-3c37ffd5f8b2", "Manager");
-staff("RealMaki", "9fdc00e0-97ca-4600-9e45-f9a27982950a", "Manager");
-staff("JedanJeLeXa", "c671c44d-a56d-4088-a7f9-8b9a23fc59fb", "Manager");
-staff("TheLuka", "0b190e82-fe6f-40d3-80d3-efc87c93b57b", "Admin");
-staff("Edo37PvP", "9fdc000-97ca-4600-9e45-f9a27982950a", "Admin");
-staff("Cuckovic", "9fdc000-97ca-4600-9e45-f9a27982950a", "Senior Mod");
-
+staff("DoktorSycolix", "e8a06a52-a151-4008-aed4-d08f2ca65fb3", "Owner");
+staff("JovanStar", "d1697264-9b04-4bdc-9d3f-b45977619cbf", "Owner");
+staff("1_1_1", "a0c43353-e0ba-45b1-b5ca-cd5e48f6461f", "Manager");
+staff("Lilconnect_", "27aaa876-7423-4932-a9ad-1d8786ddfd4b", "Manager");
+staff("Bvjkwse", "9fdc00e0-97ca-4600-9e45-f9a27982950a", "Admin");
+staff("Rasule181", "c671c44d-a56d-4088-a7f9-8b9a23fc59fb", "Mod");
+staff("cabrfyy", "0b190e82-fe6f-40d3-80d3-efc87c93b57b", "Mod");
 
 vote("Planet-Minecraft", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://planetminecraft.com");
 vote("Minecraft Servers", "https://images.app.goo.gl/oTegq49com6d3o7o9", "https://minecraftservers.org/");
